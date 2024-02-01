eb3654275abdd68da67fe2dced54d93ccbc80d4e
hanuman <hanuman.sharma@valamis.com>
2024-02-01 20:59:41 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+
+//Connect MongoDB to databse
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected successfully!");
+  })
+  .catch((err) => console.log(err));
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000!");
