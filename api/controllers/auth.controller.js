2bd97f67859a38f1917598cdb1845bbd254f36cb
hanuman <hanuman.sharma@valamis.com>
2024-02-02 14:23:48 +0530

    add a middleware and a function to handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
-export const signUp = async (req, res) => {
+export const signUp = async (req, res, next) => {
   let { username, email, password } = req.body;
   try {
     if (!username || !email || !password || username === "" || email === "" || password === "") {
-      res.status(400).json({ message: "All fields are required." });
+      next(errorHandler(400, "All fields are required"));
     }
     const userExits = await User.findOne({ email: req.body.email });
     if (userExits) {
@@ -19,6 +20,6 @@ export const signUp = async (req, res) => {
     await newUser.save();
     res.status(200).json({ message: "Signup successfully.", success: true });
   } catch (error) {
-    res.status(500).json({ message: "Internal server error", success: false, error });
+    next(error);
   }
 };
--- api/index.js
+++ api/index.js
@@ -8,8 +8,6 @@ app.use(express.json());
 //Import routes
 import authRoute from "./routes/auth.routes.js";
 
-app.use("/api/auth", authRoute);
-
 //Connect MongoDB to databse
 mongoose
   .connect(process.env.MONGO_URI)
@@ -20,4 +18,12 @@ mongoose
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000!");
+});
+
+app.use("/api/auth", authRoute);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  res.status(statusCode).json({ success: false, statusCode, message });
 });
