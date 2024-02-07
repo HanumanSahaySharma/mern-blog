ace9f37f6955398527111b2d4f23de3114488dda
hanuman <hanuman.sharma@valamis.com>
2024-02-07 14:50:20 +0530

    Footer updated

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,18 @@
-import React from "react";
-
-export default function Footer() {
+import { Footer } from "flowbite-react";
+import { BsFacebook, BsLinkedin, BsGithub } from "react-icons/bs";
+export default function footer() {
   return (
-    <footer className="w-full bg-white py-8 border-t border-t-slate-300">
+    <Footer className="bg-white text-white rounded-none shadow-none border-t border-t-slate-300">
       <div className="container mx-auto max-w-[1480px] pl-8 pr-8">
-        <p className="text-center">&copy; {new Date().getFullYear()}. All right reserved.</p>
+        <div className="flex items-center justify-between py-8 pb-4">
+          <Footer.Copyright href="#" by="Bloggy" year={new Date().getFullYear()} />
+          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsLinkedin} />
+            <Footer.Icon href="#" icon={BsGithub} />
+          </div>
+        </div>
       </div>
-    </footer>
+    </Footer>
   );
 }
