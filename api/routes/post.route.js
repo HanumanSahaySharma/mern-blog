9a8eee9d8a2b41ed38501d0d0b1612d6460465a0
hanuman <hanuman.sharma@valamis.com>
2024-02-21 20:56:52 +0530

    create get posts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
 import { verifyToken } from "../utils/verifyUser.js";
-import { create } from "../controllers/post.controller.js";
+import { create, getPosts } from "../controllers/post.controller.js";
 
 router.post("/create", verifyToken, create);
+router.get("/get-posts", getPosts);
 
 export default router;
