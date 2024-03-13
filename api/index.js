ebdc1d08d227a9e3b98ed76273d4bdb84a971d3e
hanuman <hanuman.sharma@valamis.com>
2024-03-13 14:30:51 +0530

    add pre installation for app

--- api/index.js
+++ api/index.js
@@ -2,10 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
+
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
+const __dirname = path.resolve();
+console.log(__dirname);
 
 //Import routes
 import authRoute from "./routes/auth.route.js";
@@ -27,9 +32,14 @@ app.listen(5000, () => {
 
 app.use("/api/auth", authRoute);
 app.use("/api/auth/user", userRoute);
-
 app.use("/api/post", postRoute);
 app.use("/api/comment", commentRoute);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
